bb277b4d7ce1b6920394270f7c79c4eb52aa8430
An1MuS1111 <khalidrafi1111@gmail.com>
2023-03-04 05:25:59 +0800

    adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Section1 from "./components/Section1.jsx";
+import Section2 from "./components/Section2.jsx";
 
 export default function App() {
     return (
         <div>
             <Navbar/>
             <Section1/>
+            <Section2/>
         </div>
     )
 }
--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -5,12 +5,29 @@ export default function () {
     return (
         <div className="Section2">
             <div className="intro__left">
-                <p className="Biography"></p>
-                <p className="Bio__des">Work for money and design for love! I’m Bruno, an UI/UX Interaction Designer
-                    based in Poland.</p>
+                <div className="left__sec">
+                    <p className="content--head">BIOGRAPHY</p>
+                    <p className="__des">Work for money and<br/>design for love! I’m Bruno,<br/>an UI/UX Interaction<br/>Designer
+                        based in Poland.</p>
+                </div>
+                <div className="left__sec">
+                    <p className="content--head">CONTACT</p>
+                    <p className="__des">Warsaw, Poland<br/>
+                        hello@brunoerdison.com<br/>+68 3568 89 86</p>
+                </div>
+                <div className="left__sec">
+                    <p className="content--head">SERVICES</p>
+                    <p className="__des">Website Design<br/>Mobile Application Design<br/>Animation</p>
+                </div>
+
+
+
+
             </div>
             <div className="intro__photo">
-
+                <div className="intro__photo-sec">
+                    <img src="" alt=""/>
+                </div>
             </div>
             <div className="intro__right">
 
