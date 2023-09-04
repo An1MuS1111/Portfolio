9b6958068135f8db0e585acded99e2cef601efb3
An1MuS1111 <khalidrafi1111@gmail.com>
2023-09-04 20:50:47 +0600

    technical-added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from './Components/Hero/Hero'
+import Technical from "./Components/Technical/Technical";
 
 const App = () => {
     return (
         <div>
             <Navbar />
             <Hero />
+            <Technical />
         </div>
     )
 }
---
+++ Frontend/src/Components/Technical/Technical.jsx
@@ -0,0 +1,12 @@
+import './Technical.css'
+
+const Technical = () => {
+    return (
+        <div>
+            This is Technical
+        </div>
+    )
+}
+
+
+export default Technical
