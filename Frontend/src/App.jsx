f32671e09459e0729617dd3ee9a73069cdd1992b
An1MuS1111 <khalidrafi1111@gmail.com>
2023-09-04 23:10:47 +0600

    technical props added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
+
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from './Components/Hero/Hero'
 import Technical from "./Components/Technical/Technical";
 
+
+
+
+
+
 const App = () => {
+
+    const [tools, setTools] = useState([{
+        id: 1,
+        name: 'Adobe XD',
+        description: 'Issue tracking tool that helps product managers track the progress of the product development',
+
+    }, {
+        id: 2,
+        name: 'Adobe XD',
+        description: 'Issue tracking tool that helps product managers track the progress of the product development'
+    }, {
+        id: 3,
+        name: 'Adobe Dick',
+        description: 'Issue tracking tool that helps product managers track the progress of the product development'
+    }
+    ])
     return (
         <div>
             <Navbar />
             <Hero />
-            <Technical />
+            <Technical tools={tools} />
         </div>
     )
 }
--- Frontend/src/Components/Technical/Technical.jsx
+++ Frontend/src/Components/Technical/Technical.jsx
@@ -1,9 +1,23 @@
 import './Technical.css'
 
-const Technical = () => {
+const Technical = (props) => {
+
+
     return (
-        <div>
-            This is Technical
+        <div className='section__technical'>
+            <p className='sect__tech__title'>Hand-on Experience on Tools below!</p>
+            <p className='sect__tect__desc'>This tools are used by product managers in daily basis, and our program is designed in a way that you will learn how to used this tools and when to implement this tools in real life projects</p>
+            <div className='sect__tech__tools'>
+                {props.tools.map((tool) => {
+                    return (
+                        <div>
+                            <img src="" alt="" />
+                            <p>{tool.name}</p>
+                            <p>{tool.description}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
