2b2180167a266923a4f3410dff68a1c8df606035
An1MuS1111 <khalidrafi1111@gmail.com>
2023-09-05 13:29:34 +0600

    backend-started

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+const app = express()
+app.use(cors())
+app.use(express.json)
+
+
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => console.log('Mongodb database connection extablished successfully'));
+
+PORT = 4445
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+})
